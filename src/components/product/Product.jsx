08d2b70ebd7cdb4591d895a4db34d3b61b4dac05
magasov <77aushev@mail.ru>
2024-11-19 21:37:53 +0300

    magasov

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,15 +1,25 @@
+import React from "react";
+
 import "./product.scss";
 
 const Product = () => {
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://66ea9bdb55ad32cda479a3ae.mockapi.io/items").then((res) => {
+      return res.json().then((res) => setData(res));
+    });
+  }, []);
   return (
     <>
-      <div className="product"></div>
-      <div className="product"></div>
-      <div className="product"></div>
-      <div className="product"></div>
-      <div className="product"></div>
-      <div className="product"></div>
-      <div className="product"></div>
+      {data.map((items) => (
+        <div className="product" key={items.id}>
+          <img src={items.image} alt="images" />
+          <h3>{items.title}</h3>
+          <p>{items.desc}</p>
+          <h4>{items.price} ₽</h4>
+        </div>
+      ))}
     </>
   );
 };
