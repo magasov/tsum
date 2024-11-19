c87425526a172c47134d80fb290ca9a2af4c71c7
magasov <77aushev@mail.ru>
2024-11-19 21:57:44 +0300

    magasov

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-
 import "./product.scss";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarIcon from "@mui/icons-material/Star";
 
 const Product = () => {
   const [data, setData] = React.useState([]);
+  const [fav, setFav] = React.useState({});
+
+  const onClickFav = (id) => {
+    setFav((prevFav) => ({
+      ...prevFav,
+      [id]: !prevFav[id],
+    }));
+  };
 
   React.useEffect(() => {
     fetch("https://66ea9bdb55ad32cda479a3ae.mockapi.io/items").then((res) => {
       return res.json().then((res) => setData(res));
     });
   }, []);
+
   return (
     <>
       {data.map((items) => (
@@ -19,7 +28,17 @@ const Product = () => {
           <h3>{items.title}</h3>
           <p>{items.desc}</p>
           <h4>{items.price} ₽</h4>
-          <StarBorderIcon />
+          {fav[items.id] ? (
+            <StarIcon
+              onClick={() => onClickFav(items.id)}
+              className="product__icon"
+            />
+          ) : (
+            <StarBorderIcon
+              onClick={() => onClickFav(items.id)}
+              className="product__icon"
+            />
+          )}
         </div>
       ))}
     </>
