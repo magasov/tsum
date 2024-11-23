6ce5158f7043fc564a5c11c662b07b4212fca735
Ahmed <amartazanov892@gmail.com>
2024-11-23 10:56:27 +0300

    main amart commit 4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import Footer from "./components/footer/Footer";
 
 import App from "./App";
 import "./styles/default.scss";
