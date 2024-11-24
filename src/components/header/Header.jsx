b7d5c46d13a89fa9d9adb745e64a62f407d6490e
Poteryavshiy <yandiyev.01@mail.ru>
2024-11-24 23:20:12 +0300

    Profile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,26 @@
-import "./header.scss";
+// import React from "react";
+// import "./header.scss";
+// import Profile from "../profile/Profile";
 
-const Header = () => {
-  return (
-    <div className="content">
-      <header className="header"></header>
-    </div>
-  );
-};
+// const Header = () => {
 
-export default Header;
+//   const [open, setOpen] = React.useState(false)  
+//   return (
+//     <div className="content">
+//       <header className="header">
+//         <ul>
+//           <li>Search</li>
+//           <li><span onClick={() => Profile(!open)} style={{ cursor: 'pointer'}}>
+//             Profile
+//               </span></li>
+//           <li>
+//             Faforit
+//               </li>
+//           <li>Back</li>
+//         </ul>
+//       </header>
+//     </div>
+//   );
+// };
+
+// export default Header;
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+// import "./profile.scss";
+// const Profile = ({open}) => {
+//     return (
+//         <div className="mainWindow">
+//             <div className="contentWindow">
+//                 <span className="close" onClick={open}></span>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Profile;
